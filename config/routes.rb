4e34cfe073c14bd8a84108f02df0b34edf3e2d8b
sannnann131 <tetsu09221991@gmaill.com>
2020-11-16 00:00:13 +0900

    ビュー、route調整

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,4 @@
 Rails.application.routes.draw do
-  get 'quizs5/show'
-  get 'quizs4/show'
-  get 'quizs3/show'
-  get 'quizs2/show'
-  get 'quizs1/show'
   get 'questions/index'
   root to: 'questions#index'
   resources :questions,only: [:show] do
